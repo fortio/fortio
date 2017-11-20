664e1e52edef732641cd35a30efecf4816f1e153
Bochun Zhang <bochun@google.com>
2017-11-19 17:32:50 -0800

    fix webtest

--- Webtest.sh
+++ Webtest.sh
@@ -19,13 +19,13 @@ CURL="docker run --network container:fortio_server appropriate/curl --retry 10 -
 # Check we can connect, and run a QPS test against ourselves through fetch
 $CURL -f "$BASE_URL/fortio/fetch/localhost:8080/fortio/?url=http://localhost:8080/debug&load=Start&qps=-1&json=on" | grep ActualQPS
 # Check we get the logo
-LOGO_TYPE=$($CURL -o /dev/null -f -w '%{content_type}' "$BASE_URL/fortio/logo.svg")
+LOGO_TYPE=$($CURL -o /dev/null -f -w '%{content_type}' "$BASE_URL/static/img/logo.svg")
 if [ "$LOGO_TYPE" != "image/svg+xml" ]; then
   echo "Unexpected content type for the logo: $LOGO_TYPE"
   exit 1
 fi
 # Check we can get the JS file through the proxy and it's > 50k
-SIZE=$($CURL -v -f "$BASE_URL/fortio/fetch/localhost:8080/fortio/Chart.min.js" |wc -c)
+SIZE=$($CURL -v -f "$BASE_URL/fortio/fetch/localhost:8080/static/js/Chart.min.js" |wc -c)
 if [ "$SIZE" -lt 50000 ]; then
   echo "Too small fetch for js: $SIZE"
   exit 1
