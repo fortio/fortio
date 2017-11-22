20ed35026e1760d3c8b3e9b9207a8404850f099d
Laurent Demailly <ldemailly@google.com>
2017-11-21 17:15:35 -0800

    Fully switch test to use the alternate prefix

--- Webtest.sh
+++ Webtest.sh
@@ -18,15 +18,15 @@ BASE_FORTIO="$BASE_URL$FORTIO_UI_PREFIX"
 docker exec fortio_server /usr/local/bin/fortio load -curl -stdclient https://istio.io/robots.txt
 CURL="docker exec fortio_server /usr/local/bin/fortio load -curl"
 # Check we can connect, and run a QPS test against ourselves through fetch
-$CURL "${BASE_FORTIO}fetch/localhost:8080/fortio/?url=http://localhost:8080/debug&load=Start&qps=-1&json=on" | grep ActualQPS
-# Check we get the logo
-LOGO_TYPE=$($CURL "${BASE_FORTIO}static/img/logo.svg" | grep Content-Type:)
-if [ "$LOGO_TYPE" != "Content-Type: image/svg+xml" ]; then
+$CURL "${BASE_FORTIO}fetch/localhost:8080$FORTIO_UI_PREFIX?url=http://localhost:8080/debug&load=Start&qps=-1&json=on" | grep ActualQPS
+# Check we get the logo (need to remove the CR from raw headers)
+LOGO_TYPE=$($CURL "${BASE_FORTIO}static/img/logo.svg" | grep -i Content-Type: | tr -d '\r'| awk '{print $2}')
+if [ "$LOGO_TYPE" != "image/svg+xml" ]; then
   echo "Unexpected content type for the logo: $LOGO_TYPE"
   exit 1
 fi
 # Check we can get the JS file through the proxy and it's > 50k
-SIZE=$($CURL "${BASE_FORTIO}fetch/localhost:8080/fortio/static/js/Chart.min.js" |wc -c)
+SIZE=$($CURL "${BASE_FORTIO}fetch/localhost:8080${FORTIO_UI_PREFIX}static/js/Chart.min.js" |wc -c)
 if [ "$SIZE" -lt 50000 ]; then
   echo "Too small fetch for js: $SIZE"
   exit 1
