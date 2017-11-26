bd9d5263623bc9374257d03f807e7438bd75dec3
Laurent Demailly <ldemailly@google.com>
2017-11-25 22:34:35 -0800

    Bumping to 0.4.0
    Given the big changes in this PR (and then we’re early for Dec release)

--- periodic/periodic.go
+++ periodic/periodic.go
@@ -37,7 +37,7 @@ import (
 
 const (
 	// Version is the overall package version (used to version json output too).
-	Version = "0.3.9"
+	Version = "0.4.0"
 )
 
 // DefaultRunnerOptions are the default values for options (do not mutate!).
