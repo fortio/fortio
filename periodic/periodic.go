873d9b3209e622af09645b9abd0371305612ad44
Laurent Demailly <ldemailly@google.com>
2017-12-14 20:05:53 -0800

    Added error % in title and made infinite duration UI

--- periodic/periodic.go
+++ periodic/periodic.go
@@ -37,7 +37,7 @@ import (
 
 const (
 	// Version is the overall package version (used to version json output too).
-	Version = "0.4.3"
+	Version = "0.4.4"
 )
 
 // DefaultRunnerOptions are the default values for options (do not mutate!).
