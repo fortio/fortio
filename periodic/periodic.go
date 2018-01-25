de7a237d24e70d8a624f08228fd1f1d621793c42
Laurent Demailly <ldemailly@google.com>
2018-01-24 17:17:48 -0800

    Bug fix: correct data dir must be passed to ui.Sync()

--- periodic/periodic.go
+++ periodic/periodic.go
@@ -37,7 +37,7 @@ import (
 
 const (
 	// Version is the overall package version (used to version json output too).
-	Version = "0.6.5"
+	Version = "0.6.6"
 )
 
 // DefaultRunnerOptions are the default values for options (do not mutate!).
