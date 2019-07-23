ba6c05157aa3689da1b7b293370663ef61a4e4c6
Saim Salman <saimsalman@google.com>
2019-07-23 13:41:37 -0700

    Updated comment

--- stats/stats.go
+++ stats/stats.go
@@ -518,6 +518,7 @@ func (h *Histogram) copyHDataFrom(src *Histogram) {
 
 // MergeHistData merges two different HistogramData's.
 // The function calls Import on both HistogramData's.
+// It uses Histogram's Merge Function.
 func MergeHistData(hd1 *HistogramData, hd2 *HistogramData, percList []float64) *HistogramData {
 	h1 := hd1.Import()
 	h2 := hd2.Import()
