47178bb19af662e7bddc88749f1924ede5425054
Laurent Demailly <ldemailly@roku.com>
2022-02-27 13:45:04 -0800

    adding missing uniform flag from previous mr to results. adding logger info as well.

--- periodic/periodic.go
+++ periodic/periodic.go
@@ -159,7 +159,9 @@ type RunnerResults struct {
 	DurationHistogram *stats.HistogramData
 	Exactly           int64 // Echo back the requested count
 	Jitter            bool
-	RunID             int64 // Echo back the optional run id.
+	Uniform           bool
+	RunID             int64 // Echo back the optional run id
+	AccessLoggerInfo  string
 }
 
 // HasRunnerResult is the interface implictly implemented by HTTPRunnerResults
@@ -477,10 +479,14 @@ func (r *periodicRunner) Run() RunnerResults {
 	if useExactly && actualCount != r.Exactly {
 		requestedDuration += fmt.Sprintf(", interrupted after %d", actualCount)
 	}
+	var loggerInfo string
+	if r.AccessLogger != nil {
+		loggerInfo = r.AccessLogger.Info()
+	}
 	result := RunnerResults{
 		r.RunType, r.Labels, start, requestedQPS, requestedDuration,
 		actualQPS, elapsed, r.NumThreads, version.Short(), functionDuration.Export().CalcPercentiles(r.Percentiles),
-		r.Exactly, r.Jitter, r.RunID,
+		r.Exactly, r.Jitter, r.Uniform, r.RunID, loggerInfo,
 	}
 	if log.Log(log.Warning) {
 		result.DurationHistogram.Print(r.Out, "Aggregated Function Time")
