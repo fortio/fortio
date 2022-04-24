12e1d94f3499190340de58b2510ba75a48c7b0c4
Laurent Demailly <ldemailly@gmail.com>
2022-04-24 15:12:27 -0700

    better fix for release-test in multi arch world

--- Webtest.sh
+++ Webtest.sh
@@ -14,7 +14,9 @@
 # limitations under the License.
 set -x
 # Check we can build the image
-make docker-internal TAG=webtest BUILDX_PLATFORMS="$(make docker-default-platform)" || exit 1
+NATIVE_PLATFORM="$(make docker-default-platform)"
+echo "Building for $NATIVE_PLATFORM"
+make docker-internal TAG=webtest BUILDX_PLATFORMS="$NATIVE_PLATFORM" || exit 1
 FORTIO_UI_PREFIX=/newprefix/ # test the non default prefix (not /fortio/)
 FILE_LIMIT=25 # must be low to detect leaks, go 1.14 seems to need more than go1.8 (!)
 LOGLEVEL=info # change to debug to debug
