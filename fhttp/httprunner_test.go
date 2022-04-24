a72f72f188b60398bd410091a839a9285b05d839
Laurent Demailly <ldemailly@gmail.com>
2022-04-24 13:26:38 -0700

    test complexity is fine

--- fhttp/httprunner_test.go
+++ fhttp/httprunner_test.go
@@ -251,6 +251,7 @@ func gUnzipData(t *testing.T, data []byte) (resData []byte) {
 	return
 }
 
+// nolint: gocognit
 func TestAccessLog(t *testing.T) {
 	mux, addr := DynamicHTTPServer(false)
 	mux.HandleFunc("/echo-for-alog/", EchoHandler)
