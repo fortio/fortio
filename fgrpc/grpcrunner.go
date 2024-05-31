7ce8d752deba906a0715926bdabf2cfc194a981f
KT <39397413+ktalg@users.noreply.github.com>
2024-05-31 16:57:50 +0800

    fix lint

--- fgrpc/grpcrunner.go
+++ fgrpc/grpcrunner.go
@@ -138,7 +138,7 @@ type GRPCRunnerOptions struct {
 
 // RunGRPCTest runs an http test and returns the aggregated stats.
 //
-//nolint:funlen, gocognit
+//nolint:funlen, gocognit, gocyclo
 func RunGRPCTest(o *GRPCRunnerOptions) (*GRPCRunnerResults, error) {
 	if o.Streams < 1 {
 		o.Streams = 1
