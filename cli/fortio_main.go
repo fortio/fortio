b1946305fba84136e13331b28e3d11956e9d356d
Laurent Demailly <ldemailly@gmail.com>
2025-04-02 19:08:47 -0700

    Add extensions and history

--- cli/fortio_main.go
+++ cli/fortio_main.go
@@ -27,6 +27,7 @@ import (
 	"io"
 	"os"
 	"path"
+	"path/filepath"
 	"runtime"
 	"strings"
 	"time"
@@ -47,6 +48,7 @@ import (
 	"fortio.org/safecast"
 	"fortio.org/scli"
 	"grol.io/grol/eval"
+	"grol.io/grol/extensions"
 	"grol.io/grol/repl"
 )
 
@@ -281,16 +283,33 @@ func FortioMain(hook bincommon.FortioHook) int {
 }
 
 func scriptMode() int {
-	options := repl.Options{
-		ShowEval:    true,
-		HistoryFile: "",
-	}
-	s := eval.NewState()
-
 	// we already have either 0 or exactly 1 argument from the flag parsing.
 	interactive := len(flag.Args()) == 0
+	options := repl.Options{
+		ShowEval: true,
+	}
+	// TODO: Carry some flags from the grol binary rather than hardcoded "safe"-ish config here.
+	c := extensions.Config{
+		HasLoad:           true,
+		HasSave:           interactive,
+		UnrestrictedIOs:   interactive,
+		LoadSaveEmptyOnly: false,
+	}
+	err := extensions.Init(&c)
+	if err != nil {
+		return log.FErrf("Error initializing extensions: %v", err)
+	}
 	if interactive {
-		log.Infof("Starting interactive grol script mode")
+		// Maybe move some of the logic to grol package? (it's copied from grol's main for now)
+		homeDir, err := os.UserHomeDir()
+		histFile := filepath.Join(homeDir, ".fortio_history")
+		if err != nil {
+			log.Warnf("Couldn't get user home dir: %v", err)
+			histFile = ""
+		}
+		options.HistoryFile = histFile
+		options.MaxHistory = 99
+		log.Printf("Starting interactive grol script mode")
 		return repl.Interactive(options)
 	}
 	scriptFile := flag.Arg(0)
@@ -303,6 +322,7 @@ func scriptMode() int {
 		defer f.Close()
 		reader = f
 	}
+	s := eval.NewState()
 	errs := repl.EvalAll(s, reader, os.Stdout, options)
 	if len(errs) > 0 {
 		return log.FErrf("Errors: %v", errs)
